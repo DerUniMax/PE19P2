30e29a852a97efa6ad93f96783971e89ab2c41e7
DonRohan <don.rohan@gmx.de>
2021-12-19 13:36:52 +0100

    smol cleanup

--- net_parser_pgmpy.py
+++ net_parser_pgmpy.py
@@ -1,70 +1,28 @@
+from edgesAndNodes import edges, nodes
 import pandas as pd
 from pgmpy.models import BayesianNetwork
+from pgmpy.estimators import BayesianEstimator
+import networkx as nx
+import matplotlib.pyplot as plt
+from pgmpy.estimators import MaximumLikelihoodEstimator
 
 df = pd.read_csv("P1_DP13_Wohnungen_X.csv", sep=";")
 
-edges = [
-  ('Quadratmeter', 'Zimmerzahl'),
-         ('Quadratmeter', 'Miete'),
-         ('Garage', 'Miete'),
-         ('Stockwerk', 'Aufzug'),
-         ('Stockwerk', 'Terrasse'),
-         ('Alter', 'Kueche'),
-         ('Alter', 'Stockwerk'),
-         ('Balkon', 'Terrasse'),
-         ('Lage', 'S-Bahn'),
-         ('Lage', 'Entfernung'),
-         ('Lage', 'Kindergarten'),
-         ('Lage', 'Schule'),
-         ('Miete', 'Kaution'),
-         ('Kueche', 'Kaution'),
-         ('Hausmeister', 'Nebenkosten'),
-         ('Hausmeister', 'Kehrwoche'),
-         ('Nebenkosten', 'Kleinfamilie'),
-         ('Zimmerzahl', 'Kleinfamilie'),
-         ('Kaution', 'Kleinfamilie'),
-         ('Miete', 'Kleinfamilie'),
-         ('Bad', 'Kleinfamilie'),
-         ('Kueche', 'Kleinfamilie'),
-         ('Kindergarten', 'Kleinfamilie'),
-         ('Schule', 'Kleinfamilie'),
-         ('Miete', 'Studierende'),
-         ('Kaution', 'Studierende'),
-         ('Nebenkosten', 'Studierende'),
-         ('Entfernung', 'Studierende'),
-         ('S-Bahn', 'Studierende'),
-         ('Kueche', 'Expatriate'),
-         ('Bad', 'Expatriate'),
-         ('S-Bahn', 'Expatriate'),
-         ('Entfernung', 'Expatriate'),
-         ('Kehrwoche', 'Expatriate'),
-         ('Hausmeister', 'Expatriate'),
-         ('Miete', 'Rentnerpaar'),
-         ('Kueche', 'Rentnerpaar'),
-         ('Stockwerk', 'Rentnerpaar'),
-         ('Aufzug', 'Rentnerpaar'),
-         ('Garage', 'Rentnerpaar'),
-         ('Balkon', 'Rentnerpaar'),
-         ('Terrasse', 'Rentnerpaar'),
-         ('Bad', 'Rentnerpaar'),
-         ('Bad', 'DINK'),
-         ('Kueche', 'DINK'),
-         ('Balkon', 'DINK'),
-         ('Terrasse', 'DINK'),
-         ('S-Bahn', 'DINK'),
-         ('Garage', 'DINK'),
-         ('Entfernung', 'DINK'),
-         ('S-Bahn', 'SingleHighIncome'),
-         ('Entfernung', 'SingleHighIncome'),
-         ('Garage', 'SingleHighIncome'),
-         ('Balkon', 'SingleHighIncome'),
-         ('Terrasse', 'SingleHighIncome'),
-         ('Bad', 'SingleHighIncome'),
-         ('Kueche', 'SingleHighIncome')
-         ]
-
 net = BayesianNetwork(edges)
 
-net.fit(df, n_jobs=10)
+# net.fit(df, n_jobs=10)
+# print(net)
 
-print(net)
+estimator = BayesianEstimator(net, df)
+
+# schnell für alle, falsche estimates, immer gleich 50%
+for node in nodes:
+	CPD = estimator.estimate_cpd(node, prior_type='BDeu')
+	print(CPD)
+
+# braucht allein für die Kleinfamilie ewig
+# likelihood = MaximumLikelihoodEstimator(net, df).estimate_cpd('Kleinfamilie')
+# print(likelihood)
+
+nx.draw_kamada_kawai(net, with_labels=True)
+plt.show()
